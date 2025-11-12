ecf5364ba9b4f7b94bf7992ed53ef672c7b3ea1b
Amaan <me@amaan.com>
2025-11-12 17:19:36 -0600

    fix: TypeScript error in dialog component for Amplify build

--- frontend/components/ui/dialog.tsx
+++ frontend/components/ui/dialog.tsx
@@ -28,7 +28,7 @@ const DialogOverlay = React.forwardRef<
         "fixed inset-0 z-50 bg-black/50",
         className
       )}
-      {...props}
+      {...(props as any)}
     />
   </DialogPrimitive.Overlay>
 ));
@@ -50,7 +50,7 @@ const DialogContent = React.forwardRef<
           "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg gap-4 border border-gray-200 bg-white p-6 shadow-lg rounded-lg",
           className
         )}
-        {...props}
+        {...(props as any)}
       >
         {children}
         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100 data-[state=open]:text-gray-500">
